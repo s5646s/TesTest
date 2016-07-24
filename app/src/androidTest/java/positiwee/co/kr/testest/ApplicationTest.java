ccd1f3236c15d6e9a5df96800f0bd4f95b1eab0b
positiwee <positiwee@gmail.com>
2016-07-24 13:55:20 +0900

    Initial commit

---
+++ app/src/androidTest/java/positiwee/co/kr/testest/ApplicationTest.java
@@ -0,0 +1,13 @@
+package positiwee.co.kr.testest;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
